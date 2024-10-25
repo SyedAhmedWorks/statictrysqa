4dce8bb1c236b41f48536638ed6cb27db1f0b4bb
Ahmed Hashmi <ahmed.hashmi@contour-software.com>
2024-10-25 15:26:55 +0500

    New Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TodoComponent } from './todo/todo.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'statictry';
+  title = 'todo-app';
 }
